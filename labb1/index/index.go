b1db929263028b3c26c38fa67fcbaf70c5a1a8f1
Max Nordlund <max.nordlund@gmail.com>
2014-09-15 23:35:57 +0200

    Add NewFileIndex using the lexer

--- labb1/index/index.go
+++ labb1/index/index.go
@@ -1,12 +1,35 @@
 package index
 
-import ()
+import (
+	"github.com/maxnordlund/adk/labb1/lexer"
+	"io"
+	"os"
+)
 
 // Pointer within a file
 type filePointer uint64
 
 // Index of word positions in the lexicon file
 type fileIndex map[string][]filePointer
+
+func NewFileIndex(name string) (fi fileIndex, err error) {
+	korpus, err := os.Open(name)
+	if err != nil {
+		return
+	}
+	tokenizer := lexer.New(korpus)
+	fi = make(fileIndex)
+
+	for position := 0; err == nil; advance, word, err := tokenizer.ReadToken() {
+		position += advance
+		fi.add(word, position)
+	}
+	if err != io.EOF {
+		return
+	}
+	err = korpus.Close()
+	return
+}
 
 // Adds a lexicon position for a word
 func (fi fileIndex) add(word string, position filePointer) {
