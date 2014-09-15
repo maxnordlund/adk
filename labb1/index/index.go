3eac3d8b3576b657f13125760130be23d64a0f07
Max Nordlund <max.nordlund@gmail.com>
2014-09-15 23:38:03 +0200

    Add NewSearchIndex using fileIndex

--- labb1/index/index.go
+++ labb1/index/index.go
@@ -1,10 +1,14 @@
 package index
 
 import (
+	"bytes"
+	"encoding/gob"
 	"github.com/maxnordlund/adk/labb1/lexer"
 	"io"
 	"os"
 )
+
+const SIZE = 8
 
 // Pointer within a file
 type filePointer uint64
@@ -38,6 +42,35 @@ func (fi fileIndex) add(word string, position filePointer) {
 
 // Index of word positions in the index file
 type searchIndex map[string]filePointer
+
+func NewSearchIndex(name, fi fileIndex) (si searchIndex, err error) {
+	index, err := os.Open("data/index")
+	if err != nil {
+		return
+	}
+	si = make(searchIndex)
+	position := 0
+	for word, positions := range fi {
+		buf := bytes.NewBuffer(make([]byte, 0, len(word)+len(positions)*SIZE))
+		enc := gob.NewEncoder(buf)
+		err = enc.Encode(word)
+		if err != nil {
+			return
+		}
+		err = enc.Encode(positions)
+		if err != nil {
+			return
+		}
+		n, err := index.Write(buf.Bytes())
+		if err != nil {
+			return
+		}
+		si.add(word, position)
+		position += n
+	}
+	err = index.Close()
+	return
+}
 
 // Adds a index position for a word
 func (si searchIndex) add(word string, position filePointer) {
