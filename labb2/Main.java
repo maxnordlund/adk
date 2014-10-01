a749202b1aa01092f1a3029e9a68358b054ced7e
Max Nordlund <max.nordlund@gmail.com>
2014-10-01 21:48:53 +0200

    Optimize charAt/String -> char[]

--- labb2/Main.java
+++ labb2/Main.java
@@ -5,50 +5,54 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
   private static int[][] matrix;
 
+  // Kattis promise to not have word longer then 40 characters long
+  private static final int MAX_LENGTH = 41;
+
   public static void main(String args[]) throws IOException {
+    int i;
+    int j;
+
     BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
     // Säkrast att specificera att UTF-8 ska användas, för vissa system har annan
     // standardinställning för teckenkodningen.
 
     int maxLength = Integer.MIN_VALUE;
-    String word = stdin.readLine();
+    char[] word = readLine(stdin);
     StringBuilder sb = new StringBuilder();
-    LinkedList<String> words = new LinkedList<String>();
-    while (!word.equals("#")) {
-      if (word.length() > maxLength) {
-        maxLength = word.length();
+    List<char[]> wordList = new ArrayList<char[]>();
+    while (word[0] != '#') {
+      if (word.length > maxLength) {
+        maxLength = word.length;
       }
-      words.add(word);
-      word = stdin.readLine();
+      wordList.add(word);
+      word = readLine(stdin);
     }
 
-    while ((word = stdin.readLine()) != null) {
-      int i;
-      int j;
-      int wordLength = word.length();
+    char[][] words = wordList.toArray(new char[0][0]);
+
+    matrix = new int[MAX_LENGTH][MAX_LENGTH];
+    for (i = 0; i < MAX_LENGTH; ++i) {
+      matrix[i][0] = matrix[0][i] = i;
+    }
+
+    while ((word = readLine(stdin)).length > 0) {
       int minDist = Integer.MAX_VALUE;
-      String oldCandidate = "";
-      LinkedList<String> minWords = new LinkedList<String>();
+      char[] oldCandidate = new char[0];
+      List<char[]> minWords = new LinkedList<char[]>();
 
-      matrix = new int[wordLength+1][maxLength+1];
-
-      for (i = 0; i <= wordLength; ++i) {
-        matrix[i][0] = i;
-      }
-      for (j = 0; j <= maxLength; ++j) {
-        matrix[0][j] = j;
-      }
-
-      for (String candidate : words) {
-        int dist = distance(word, candidate, oldCandidate, wordLength);
+      for (i = 0; i < words.length; ++i) {
+        char[] candidate = words[i];
+        int dist = distance(word, candidate, oldCandidate);
         if (dist < minDist) {
           minDist = dist;
-          minWords = new LinkedList<String>();
+          minWords = new LinkedList<char[]>();
           minWords.add(candidate);
         } else if (dist == minDist) {
           minWords.add(candidate);
@@ -60,7 +64,7 @@ public class Main {
       sb.append(" (");
       sb.append(minDist);
       sb.append(")");
-      for (String candidate : minWords) {
+      for (char[] candidate : minWords) {
         sb.append(" ");
         sb.append(candidate);
       }
@@ -70,41 +74,47 @@ public class Main {
   }
 
   // Marked `private static final` to encourage inlining
-  private static final int distance(String word, String candidate, String oldCandidate, int wordLength) {
+  private static final char[] readLine(BufferedReader stdin) throws IOException {
+    String str = stdin.readLine();
+    if (str == null) {
+      return new char[0];
+    } else {
+      return str.toCharArray();
+    }
+  }
+
+  // Marked `private static final` to encourage inlining
+  private static final int distance(char[] word, char[] candidate, char[] oldCandidate) {
     int i;
     int j;
     int start = 1;
-    int candidateLength = candidate.length();
     int min;
 
-    if (candidateLength < oldCandidate.length()) {
-      min = candidateLength;
+    if (candidate.length < oldCandidate.length) {
+      min = candidate.length;
     } else {
-      min = oldCandidate.length();
+      min = oldCandidate.length;
     }
 
-    for (i = 0; i < min; ++i) {
-      if (candidate.charAt(i) != oldCandidate.charAt(i)) {
-        break;
-      }
+    for (i = 0; i < min && candidate[i] == oldCandidate[i]; ++i) {
       ++start;
     }
 
-    for (i = 1; i <= wordLength; ++i) {
-      for (j = start; j <= candidateLength; ++j) {
+    for (i = 1; i <= word.length; ++i) {
+      for (j = start; j <= candidate.length; ++j) {
         min = matrix[i-1][j-1];
-        if (word.charAt(i-1) != candidate.charAt(j-1)) {
+        if (word[i-1] != candidate[j-1]) {
           ++min;
-        }
-        if (min > matrix[i-1][j]) {
-          min = 1 + matrix[i-1][j];
         }
         if (min > matrix[i][j-1]) {
           min = 1 + matrix[i][j-1];
         }
+        if (min > matrix[i-1][j]) {
+          min = 1 + matrix[i-1][j];
+        }
         matrix[i][j] = min;
       }
     }
-    return matrix[wordLength][candidateLength];
+    return matrix[word.length][candidate.length];
   }
 }
