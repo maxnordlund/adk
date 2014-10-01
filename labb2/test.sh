7760bee3eae47cf0604ace61bc433525a6f35f18
Max Nordlund <max.nordlund@gmail.com>
2014-10-01 21:48:16 +0200

    Remove unneeded output

--- labb2/test.sh
+++ labb2/test.sh
@@ -7,13 +7,13 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 javac -Werror Main.java || exit $?
 
 for t in testfall/testmedordlista{1,2}; do
-  echo $t
-  cat $t.indata | sed '1,/\#/d' | wc -l
+  # echo $t
+  # cat $t.indata | sed '1,/\#/d' | wc -l
   cat $t.indata | time java Main | diff - $t.utdata
 done
 
 for t in testfall/testord{1,2}; do
-  echo $t
-  cat $t.indata | wc -l
+  # echo $t
+  # cat $t.indata | wc -l
   cat ordlista.txt $t.indata | time java Main | diff - $t.utdata
 done
